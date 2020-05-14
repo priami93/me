94e86c7b889685e923937c722f964facb4183bf5
Samuele <samuelepriami@hotmail.it>
2020-05-14 23:08:56 +0200

    Updates

---
+++ precache-manifest.1e6b1181013c264e2e400a75d579335a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8383d40d895a1323c652b48f7d8f01",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "eb2435939cebfe1e5fb9",
+    "url": "/me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ee3036a4f6d50b50628",
+    "url": "/me/static/js/2.5a76fdf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.5a76fdf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2435939cebfe1e5fb9",
+    "url": "/me/static/js/main.29c8f0ab.chunk.js"
+  },
+  {
+    "revision": "300a927be0c6c06b4a9d",
+    "url": "/me/static/js/runtime-main.ae24c08c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/me/static/media/logo.5d5d9eef.svg"
+  }
+]);
